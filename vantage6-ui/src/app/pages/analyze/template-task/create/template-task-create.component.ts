7d4dc659b527e525520bd4d21cb4a13aea25bd4d
Luis Sago <lws.sago@gmail.com>
2026-07-29 10:52:36 +0200

    fix: ui: missing api path when creating tasks

--- vantage6-ui/src/app/pages/analyze/template-task/create/template-task-create.component.ts
+++ vantage6-ui/src/app/pages/analyze/template-task/create/template-task-create.component.ts
@@ -233,7 +233,7 @@ export class TemplateTaskCreateComponent implements OnInit {
       image: this.algorithm?.image || '',
       collaboration_id: this.chosenCollaborationService.collaboration$.value?.id || -1,
       store_id: this.algorithm?.algorithm_store_id || -1,
-      server_url: environment.server_url,
+      server_url: `${environment.server_url}${environment.api_path}`,
       databases: taskDatabases,
       organizations: selectedOrganizations.map((organizationID) => {
         return { id: Number.parseInt(organizationID), input: btoa(JSON.stringify(input)) || '' };
